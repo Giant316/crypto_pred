8bc4bb54ceafef05844af7c929179e47c116a1bf
Giant316 <9390561+Giant316@users.noreply.github.com>
2021-03-20 13:34:25 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Comparing model's accuracyycy for predicting cryptocurrency prices',
+    author='Jia Yan',
+    license='MIT',
+)
