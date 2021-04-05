fa16a66e36c14b6104bdaa7e21adff08d0013ea2
Giant316 <9390561+Giant316@users.noreply.github.com>
2021-04-05 22:06:11 +0200

    add config file + refine log info

--- src/models/tuning.py
+++ src/models/tuning.py
@@ -6,19 +6,9 @@ import ray
 from ray import tune
 from ray.tune.schedulers import AsyncHyperBandScheduler
 import logging 
+import configparser
 
-def train_mlp(config):
-    url = 'https://raw.githubusercontent.com/Giant316/crypto_scrapy/main/BTC.csv'
-    df = pd.read_csv(url)
-
-    # univariate time series 
-    df = df[['Close']]
-
-    # a window with number of time step of input = 6 
-    window = utils.DataGenerator(
-        input_width=6, label_width=1, offset=1, 
-        label_columns=df.columns.to_list(), df=df)
-        
+def train_mlp(config): 
     model = tf.keras.Sequential([
       tf.keras.layers.Flatten(), # convert to 1D: (time, features) => (time*features) 
       tf.keras.layers.Dense(config["hidden"], config['activation']),
@@ -34,12 +24,18 @@ def train_mlp(config):
     callbacks=[TuneReportCallback({"MAPE": "mean_absolute_percentage_error"
     })])
 
-if __name__ == "__main__":
-    exp_name = "Tune_MLP"
-    logging.basicConfig(filename= f"./reports/{exp_name}.log", format='%(asctime)s %(message)s')
-    logger = logging.getLogger() # create a logger object
-    logger.setLevel(logging.INFO)
+def read_config(no=0):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    sections = config.sections()
+    exp = sections[no]
+    dat = config[sections[no]]['data']
+    inw = config[sections[no]]['input_width']
+    lpw = config[sections[no]]['label_width']
+    ofs = config[sections[no]]['offset']
+    return exp, dat, inw, lpw, ofs
 
+def tune(exp_name, window_size):
     ray.init(num_cpus=4)# if args.smoke_test else None)
     sched = AsyncHyperBandScheduler(
         time_attr="training_iteration", max_t=400, grace_period=20)
@@ -61,8 +57,33 @@ if __name__ == "__main__":
         },
         config={
             "hidden": tune.grid_search([1, 2]),
-            "activation": tune.choice(['relu', 'linear'])
+            "activation": tune.choice(['relu', 'linear']),
+            "window": window_size
         })
-    logger.info("Best hyperparameters found were: %s", analysis.best_config)
+    return analysis
+
+if __name__ == "__main__":
+    # process configuration file
+    exp, dat, ipw, lbw, ofs = read_config(1) # get second experiment's config
+    df = pd.read_csv(dat)
+
+    # univariate time series 
+    df = df[['Close']]
+
+    # a window with number of time step of input = 6 
+    win6 = utils.DataGenerator(
+        input_width=ipw, label_width=lbw, offset=ofs, 
+        label_columns=df.columns.to_list(), df=df)
+
+    exp_name = "Tune_MLP"
+    analysis = tune(exp_name=exp_name, window_size=win6)
+
+    # log tuning results  
+    logging.basicConfig(filename= f"./reports/{exp_name}.log", format='%(asctime)s %(message)s')
+    logger = logging.getLogger() # create a logger object
+    logger.setLevel(logging.INFO)   
+    logger.info("%s with configuration of Input Width: %s", exp, ipw)
+    logger.info("results save in folder: %s", exp_name)    
+    logger.info("Best hyperparameters: Hidden: %s; Activation: %s", analysis.best_config["hidden"], analysis.best_config['activation'])
 
     ray.shutdown()
